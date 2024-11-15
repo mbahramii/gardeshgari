f77751a9c987156d9dac85d0f66f24bbf4184a80
mbahramii <bahramiimohammad92@gmail.com>
2024-11-16 03:19:10 +0330

    make SelectedArticle section in HomePage

---
+++ src/components/SelectedArticles.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import moment from "jalali-moment";
+
+const SelectedArticles = ({ id, title, imageUrl, content, createdAt }) => {
+  const navigate = useNavigate();
+
+
+  // با کلیک شما به محتوای ارتیکل میروید 
+  const handleClick = () => {
+    navigate(`/article-page/${id}`);
+  };
+
+  return (
+    <div dir="rtl"
+      onClick={handleClick}
+      className="flex bg-white text-white overflow-hidden border-b border-gray-300 cursor-pointer"
+    >
+      <div className="w-[300px] h-[129px] relative rounded-lg overflow-hidden m-3">
+        <img
+          src={imageUrl}
+          alt={title}
+          className="object-cover h-full w-full rounded-lg"
+        />
+        <div className="absolute top-2 left-2 bg-yellow-500 text-black px-2 py-1 rounded">
+            گردشگری
+        </div>
+
+    </div>   
+    </div>
+  );
+};
+
+export default SelectedArticles;
